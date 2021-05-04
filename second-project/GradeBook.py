50047b6c227304f574ab0b4feff0f6fcf52cdb5f
nikolozi2001 <34250988+nikolozi2001@users.noreply.github.com>
2021-05-04 22:40:26 +0400

    update gradebook

--- second-project/GradeBook.py
+++ second-project/GradeBook.py
@@ -4,11 +4,22 @@ grades = [98, 97, 85, 88]
 GradeBook = zip(subjects,grades)
 print(list(GradeBook))
 
+#added more subjects
+
 subjects.append("computer science")
 grades.append(100)
 
 GradeBook = zip(subjects,grades)
 print(list(GradeBook))
 
-GradeBook.append("visual arts", 93)
-print(list(GradeBook))
+grade_book = list(GradeBook)
+
+grade_book.append(["visual arts", 93])
+# (7) modify gradebook
+
+grades[4] += 5
+print(list(grade_book))
+
+#here starts last semester gradebook
+
+subjects2 = ["ethical hacking", "cyber security", "IoT", "artifical intellect"]
